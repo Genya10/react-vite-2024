fb4a4b3028c9c4178061acc7ad5e6e9aa14af6bf
Jenya <113167825+Genya10@users.noreply.github.com>
2024-06-18 21:52:17 +0300

    create queryClient and use useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 
+const getData = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  console.log(response);
+  return response.json();
+};
 
 function App() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getData,
+  });
+  console.log(error);
 
   return (
     <>
       <h1>React Vite Query</h1>
+      {isLoading
+        ? "Loading..."
+        : data?.length
+        ? data.map((post: any) => <div key={post.id}>{post.title}</div>)
+        : "Loading..."}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+//https://jsonplaceholder.typicode.com/posts
+///*eslint-disable-next-line @typescript-eslint/no-explicit-any */
