2b94e4255ad64679a4fee70e4692dd6da435c48c
Jenya <113167825+Genya10@users.noreply.github.com>
2024-06-19 20:10:13 +0300

    install and use axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,61 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const getData = async () => {
+const getData =  () => {
+  return axios.get("https://jsonplaceholder.typicode.com/posts");
+};
+
+function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getData,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Проверка на наличие данных
+  if (!data) {
+    return <div>Data not found</div>;
+  }
+
+  // Извлечение различных свойств из объекта ответа
+  const posts = data.data; // Массив постов
+  const status = data.status; // Статус ответа (например, 200)
+  const headers = data.headers; // Заголовки ответа
+  const statusText = data.statusText; // Текст статуса (например, "OK")
+
+  console.log("Status:", status);
+  console.log("Headers:", headers);
+  console.log("Status Text:", statusText);
+
+  return (
+    <>
+      <h1>React Vite Query</h1>
+      {posts.length ? (
+        posts.map((post:any) => <div key={post.id}>{post.title}</div>)
+      ) : (
+        "No posts found"
+      )}
+    </>
+  );
+  /*return (
+    <>
+      <h1>React Vite Query</h1>
+      {isLoading
+        ? "Loading..."
+        : data?.data?.length
+        ? data.data.map((post: any) => <div key={post.id}>{post.title}</div>)
+        : "Not found"}
+    </>
+  );*/
+}
+
+export default App;
+
+
+/*const getData = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   console.log(response);
   return response.json();
@@ -25,7 +80,7 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
 
 //https://jsonplaceholder.typicode.com/posts
 ///*eslint-disable-next-line @typescript-eslint/no-explicit-any */
